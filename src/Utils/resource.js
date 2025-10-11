a0db4c51150efa70094d83ac6733828e8a683a2f
irvancolz <irvansaharudin@gmail.com>
2025-10-11 16:07:03 +0700

    fix resources url

--- src/Utils/resource.js
+++ src/Utils/resource.js
@@ -2,26 +2,26 @@ export default [
   {
     type: "texture",
     name: "grass_color_texture",
-    path: "/grass.png",
+    path: "./grass.png",
   },
   {
     type: "texture",
     name: "cloud_noise_texture",
-    path: "/cloud.jpg",
+    path: "./cloud.jpg",
   },
   {
     type: "texture",
     name: "leaves_alpha_texture",
-    path: "/leaves.png",
+    path: "./leaves.png",
   },
   {
     type: "gltfModel",
     name: "statue_model",
-    path: "/statue.gltf",
+    path: "./statue.gltf",
   },
   {
     type: "gltfModel",
     name: "tree_model",
-    path: "/tree.gltf",
+    path: "./tree.gltf",
   },
 ];
